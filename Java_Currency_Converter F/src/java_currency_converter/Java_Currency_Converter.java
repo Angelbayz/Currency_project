10804e17552ec9a4a3edee2a8b8240ce4c595ae8
Angelbayz <143059250+Angelbayz@users.noreply.github.com>
2024-09-17 09:40:50 +0800

    Add files via upload

---
+++ Java_Currency_Converter F/src/java_currency_converter/Java_Currency_Converter.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_currency_converter;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Java_Currency_Converter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
